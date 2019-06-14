d042a4c6f9cb7941abd51468083ca5a335d246e0
DangJinling <dangjinling_1012@126.com>
2019-06-14 09:46:25 +0800

    Add files via upload

---
+++ leadmanager/frontend/src/actions/leads.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from './types';
+
+export const getLeads = () => dispatch => {
+    axios.get('/api/leads/')
+        .then(res => {
+            dispatch({
+                type: GET_LEADS,
+                payload: res.data
+            })
+        })
+        .catch(err => console.log(err));
+}
+
+export const deleteLead = (id) => dispatch => {
+    axios.delete(`/api/leads/${id}/`)
+        .then(() => {
+            dispatch({
+                type: DELETE_LEAD,
+                payload: id
+            })
+        })
+        .catch(err => console.log(err));
+}
+
+export const addLead = (lead) => dispatch => {
+    axios.post(`/api/leads/`, lead)
+        .then(() => {
+            dispatch({
+                type: ADD_LEAD,
+                payload: lead
+            })
+        })
+        .catch(err => console.log(err));
+}
